20682e8881c9fff6a3fb0e60766716d2d6589042
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-10 18:34:50 +0300

    Await / Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-async function asyncUploadUser() {
+export default async function asyncUploadUser() {
   try {
     let photo = await uploadPhoto();
     let user = await createUser();
