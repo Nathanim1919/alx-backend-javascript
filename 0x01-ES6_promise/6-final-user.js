6861d04deb5803ada710783b988dda541d1f4965
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-10 17:58:35 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,13 @@
-import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)]).then((re) => {
-    re.map((result) => ({
-      status: result.status,
-      value: re.status === 'fulfilled' ? re.value : String(re.reason),
-    }));
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
