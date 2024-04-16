21816b8068267c32be5ae15be6bf5c5d346e23eb
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-16 22:15:44 +0300

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,8 @@
 export default function getListStudentIds(arrays) {
-  if (arrays.isArray === false){
-    return []
-  } else {
-    return arrays.map(obj => (
-        obj.id
-    ))
+  if (arrays.isArray === false) {
+    return [];
   }
+  return arrays.map((obj) => (
+    obj.id
+  ));
 }
