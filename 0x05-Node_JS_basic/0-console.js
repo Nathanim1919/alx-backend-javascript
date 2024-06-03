263f09cb71db157c4285637b45ad9eb569fa0fae
Nathanim1919 <nathanim2tadele@gmail.com>
2024-06-03 23:19:59 +0300

    Executing basic javaScript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,13 @@
+/** 
+* Logs a message to the console
+* @param {string} message - The message to log
+*/
 function displayMessage(message){
 	console.log(message);
 }
 
+
+/**
+ * Export the function displayMessage
+ */
 module.exports = displayMessage;
