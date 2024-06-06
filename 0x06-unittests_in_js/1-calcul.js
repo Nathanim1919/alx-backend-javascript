a018f730b6dbd8c4bb61c1ea0ac8038bdd0316ac
Nathanim1919 <nathanim2tadele@gmail.com>
2024-06-06 22:58:03 +0300

    Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number or 'Error'
+ */
+
+function calculateNumber(type, a, b){
+    if (type === 'SUM'){
+        return Math.round(a) + Math.round(b);
+    } else if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+    } else if (type === 'DIVIDE') {
+        if (b === 0){
+            return 'Error';
+        } else {
+            return Math.round(a) / Math.round(b);
+        }
+    }
+}
+
+module.exports = calculateNumber
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,31 @@
+const calculateNumber = require('./1-calcul');
+const assert = require('assert')
+
+
+describe("CalculateNumber", () => {
+    describe("SUM", () => {
+        it("should return 6", () => {
+            const result = calculateNumber("SUM", 1.4, 4.5);
+            assert.strictEqual(result,6);
+        });
+    });
+
+    describe("SUBTRACT", () => {
+        it("should return -4", () => {
+            const result = calculateNumber("SUBTRACT", 1.4, 4.5);
+            assert.strictEqual(result, -4)
+        });
+    });
+
+    describe("DIVIDE", () => {
+        it("should return 0.2", () => {
+            const result = calculateNumber('DIVIDE', 1.4, 4.5)
+            assert.strictEqual(result, 0.2)
+        });
+
+        it("should return Error", () => {
+            const result = calculateNumber("DIVIDE", 1.4, 0);
+            assert.strictEqual(result, "Error")
+        });
+    });
+})
