f3077a48225bd70b471ef8e987b8406a378caeb1
Nathanim1919 <nathanim2tadele@gmail.com>
2024-06-06 23:09:15 +0300

    Combining descriptions

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,20 +6,20 @@
  * @require: the module to be tested
  */
 
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', function() {
-    it('should return 4', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-    })
-    it('should return 5', () => {
-        assert.strictEqual( calculateNumber(1, 3.7), 5);
-    });
-    it('should return 5', () => {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    });
-    it('should return 6', () => {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    });
+describe("calculateNumber", function () {
+  it("should return 4", () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it("should return 5", () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it("should return 5", () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it("should return 6", () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
