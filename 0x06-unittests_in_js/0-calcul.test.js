20ae9e69b9ae2b09b6aec7783c2ecbcbbb646f6b
Nathanim1919 <nathanim2tadele@gmail.com>
2024-06-06 22:10:21 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -10,13 +10,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
-    it('should return 5', function() {
+    it('should return 4', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    })
+    it('should return 5', () => {
         assert.strictEqual( calculateNumber(1, 3.7), 5);
     });
-    it('should return 5', function() {
+    it('should return 5', () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
-    it('should return 6', function() {
+    it('should return 6', () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
